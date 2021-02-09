cf9e1e5334c60131cda24a00d62000485ea8c87a
karnakar reddy thoorpu <thoorpukarnakar@gmail.com>
2021-02-09 10:03:41 +0530

    Update ML_packaging.py

--- packaging/ML_packaging.py
+++ packaging/ML_packaging.py
@@ -1,1 +1,6 @@
+# This Module helps to orgnanize, reuse and reproduce ML prjects in ML pipeline.
+
+# One could package ML model in two ways
+# 1. Conda (Which stores specifications in .yaml file, which is heart of the project)
+# 2. Docker
 
